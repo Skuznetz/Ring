c7a9a40f47ff49f41136991112b2670f614d86de
Skuznetz <s.kuznetzov@mail.ru>
2016-12-01 17:56:18 +0200

    добавляем валидации

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,17 +7,19 @@ class ArticlesController < ApplicationController
    def show
     @article = Article.find(params[:id])
   end
-
-	def new
-	end
-
-  def create
-    @article = Article.new(article_params)
+def new
+  @article = Article.new
+end
  
-    @article.save
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
     redirect_to @article
+  else
+    render 'new'
   end
- 
+end
   private
     def article_params
       params.require(:article).permit(:title, :text)
